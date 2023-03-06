bd1cac35320326b939b5022d9ad5f53df300913f
Denis Blondeau <dbappdev@outlook.com>
2023-03-06 14:35:51 -0500

    Initial Commit

---
+++ SonosAPIDemo/SonosAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonosAPIDemoApp.swift
+//  SonosAPIDemo
+//
+//  Created by Denis Blondeau on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct SonosAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
